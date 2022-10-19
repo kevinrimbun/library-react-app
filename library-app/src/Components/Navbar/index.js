eefb8550522c40a09fd3b834a4808f4113b711f2
kevinrimbun03 <kevinrimbun03>
2022-10-19 20:29:42 +0700

    first commit

---
+++ library-app/src/Components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BsJustify } from 'react-icons/bs';
+
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li><BsJustify /></li>
+            <li>All Categories</li>
+            <li>All Time</li>
+            
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ library-app/src/Pages/App/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from '../Home';
+import Login from '../Login';
+import Register from '../Register';
+import DetailContent from '../DetailContent';
+
+const index = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Register/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/home' element={<Home/>} />
+            <Route path='/detail-content' element={<DetailContent/>} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default index
