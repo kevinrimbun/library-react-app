1bbc8f5e880410de514e900d24b08981f8f4e116
kevinrimbun03 <kevinrimbun03>
2022-10-20 12:47:37 +0700

    add carousel

--- library-app/src/Components/Carousel/index.js
+++ library-app/src/Components/Carousel/index.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import spiderman from '../../assets/komik-spiderman.png';
+import buku from '../../assets/buku-bodo-amat.png';
+import boruto from '../../assets/komik-naruto.png';
+import '../../Style/Components/carousel.css'
+// Or for ES2015 module
+import Flickity from 'react-flickity-component'
 
-const Carousel = () => {
+const flickityOptions = {
+    initialIndex: 2,
+    wrapAround: true
+}
+
+function Carousel() {
   return (
-    <div>Carousel</div>
+    <Flickity
+      className={'carousel'} // default ''
+      elementType={'div'} // default 'div'
+      options={flickityOptions} // takes flickity options {}
+      disableImagesLoaded={false} // default false
+      reloadOnUpdate // default false
+      static // default false
+    >
+
+      <div class="gallery-cell">
+        <img src={spiderman} alt="spiderman" srcset="" />
+        <div className="position-absolute top-50 start-50 translate-middle text-carousel">
+          <h4 className="text-center"><u>Boruto: Naruto Next Generations</u></h4>
+          <p className="text-center">Mikio Ikemoto</p>
+        </div>
+      </div>
+
+      <div class="gallery-cell">
+        <img src={buku} alt="buku" srcset="" />
+        <div className="position-absolute top-50 start-50 translate-middle text-carousel">
+          <h4 className="text-center"><u>Sebuah Seni Untuk Bersikap Bodo Amat</u></h4>
+          <p className="text-center">Mark Manson</p>
+        </div>
+      </div>
+
+      <div class="gallery-cell">
+        <img src={boruto} alt="boruto" srcset="" />
+        <div className="position-absolute top-50 start-50 translate-middle text-carousel">
+          <h4 className="text-center"><u>The Amazing Spiderman</u></h4>
+          <p className="text-center ">Stan Lee</p>
+        </div>
+      </div>
+    </Flickity>
   )
 }
 
--- library-app/src/Components/Navbar/index.js
+++ library-app/src/Components/Navbar/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BsJustify, BsSearch } from 'react-icons/bs';
-import '../../Style/Components/navbar.css'
-import Logo from '../../assets/lily.png'
+import '../../Style/Components/navbar.css';
+import Logo from '../../assets/lily.png';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 
 const Navbar = () => {
@@ -11,9 +13,22 @@ const Navbar = () => {
             <div>
                 <li><BsJustify /></li>
             </div>
-            <div className='list-wrapper'> 
-                <li>All Categories</li>
-                <li>All Time</li>
+            {/* Dropdown React Bootsrap */}
+            <div className='dropdown-wrapper'>
+                <div className='dropdown-left'> 
+                    <DropdownButton id="dropdown-basic-button" title="All Categories">
+                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    </DropdownButton>
+                </div>
+                <div className='dropdown-right'>
+                    <DropdownButton id="dropdown-basic-button" title="All Time">
+                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    </DropdownButton>
+                </div>
             </div>
             <div className='search-wrapper'>
             <form className="form-inline mx-auto">
